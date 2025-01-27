8d5cc63506e4b4eff7bfaa4e446391bf2c06e757
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2025-01-28 00:10:08 +0530

    minor issue

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const api = axios.create({
-  baseURL: "https://jobportalbackend-vwx4.onrender.com/",
+  baseURL: "https://jobportalbackend-vwx4.onrender.com",
   withCredentials: true, // For handling cookies
 });
 
